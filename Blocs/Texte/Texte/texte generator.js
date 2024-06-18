e8908318f213972f7448623d0268a6f532261212
samuelriboulet <64737865+samuelriboulet@users.noreply.github.com>
2024-06-18 18:57:29 +0200

    Add files via upload

---
+++ Blocs/Texte/Texte/texte generator.js
@@ -0,0 +1,7 @@
+javascript.javascriptGenerator.forBlock['texte__texte'] = function(block, generator) {
+  var text_text = block.getFieldValue('TEXT');
+  // TODO: Assemble javascript into code variable.
+  var code = '...';
+  // TODO: Change ORDER_NONE to the correct strength.
+  return [code, Blockly.javascript.ORDER_NONE];
+};
