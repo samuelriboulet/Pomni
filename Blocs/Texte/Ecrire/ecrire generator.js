f4c2afc12e76b449fc5936d07b8cab974d64a892
samuelriboulet <64737865+samuelriboulet@users.noreply.github.com>
2024-06-23 20:25:55 +0200

    Delete Blocs directory

--- Blocs/Texte/Ecrire/ecrire generator.js
+++
@@ -1,6 +0,0 @@
-javascript.javascriptGenerator.forBlock['texte_ecrire'] = function(block, generator) {
-  var value_text = generator.valueToCode(block, 'TEXT', javascript.Order.ATOMIC);
-  // TODO: Assemble javascript into code variable.
-  var code = 'window.alert("")\n';
-  return code;
-};
